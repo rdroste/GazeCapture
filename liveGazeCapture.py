b7fc6a24c1be09bfcfaa07d1007750d64533bb3c
Richard <droste.richard@gmail.com>
2017-11-03 12:39:41 +0000

    Add main fct, edit configs

--- liveGazeCapture.py
+++ liveGazeCapture.py
@@ -3,43 +3,54 @@ import faceDetection
 import visualizeGaze
 import iTracker
 import numpy as np
+import sys
 
 MOVING_AVG = 3
 
-# Initialize iTracker and face detection
-itrack = iTracker.iTracker()
-detector = faceDetection.faceDetection()
 
-# Initialize cam
-cap = cv2.VideoCapture(0)
-ret, img = cap.read()
-cv2.namedWindow("image")
-cv2.imshow("image",img)
-cv2.waitKey(500)
+def liveGazeCapture(config):
+    # Initialize iTracker and face detection
+    itrack = iTracker.iTracker()
+    detector = faceDetection.faceDetection()
 
-# Initialize reslt visualization
-vis = visualizeGaze.visualize(0)
-
-# Initialize MA filter
-coords_array = np.zeros((3,2))
-ma_counter = 0
-
-while True:
-
+    # Initialize cam
+    cap = cv2.VideoCapture(0)
     ret, img = cap.read()
-    img = img[:,80:510,:]
+    cv2.namedWindow("image")
+    cv2.imshow("image",img)
+    cv2.waitKey(500)
 
-    images = detector.detect(img, DRAW=0)
-    if images is None:
-        continue
+    # Initialize reslt visualization
+    vis = visualizeGaze.visualize(config)
 
-    coords = itrack.infer(images)
+    # Initialize MA filter
+    coords_array = np.zeros((3,2))
+    ma_counter = 0
 
-    coords_array[ma_counter, :] = coords
-    coords_filtered = np.mean(coords_array, axis=0)
-    if ma_counter < MOVING_AVG - 1:
-        ma_counter += 1
+    while True:
+
+        ret, img = cap.read()
+        img = img[:,80:510,:]
+
+        images = detector.detect(img, DRAW=0)
+        if images is None:
+            continue
+
+        coords = itrack.infer(images)  # x, y in cm
+
+        coords_array[ma_counter, :] = coords
+        coords_filtered = np.mean(coords_array, axis=0)
+        if ma_counter < MOVING_AVG - 1:
+            ma_counter += 1
+        else:
+            ma_counter = 0
+
+        vis.showGaze(coords_filtered)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        config = map(int, sys.argv[1])[0]
     else:
-        ma_counter = 0
-
-    vis.newVis(coords_filtered)
+        config = 0
+    liveGazeCapture(config)
--- visualizeGaze.py
+++ visualizeGaze.py
@@ -4,17 +4,22 @@ import cv2
 
 class visualize:
     def __init__(self, config):
-        if config is 0:
-            screen_res = np.array([1440, 2560])
-            ul_corner = np.array([0,100])
-            lr_corner = np.array([100,100])
-            ppi = 210  # 210 / 2.54
-            header_cm = 1
-            cam2screen_cm = 1.3
 
-        self.reducedRes = screen_res - ul_corner - lr_corner
+        # deault config
+        screen_res = np.array([1440, 2560])
+        ul_corner = np.array([0,100])
+        lr_corner = np.array([100,100])
+        ppi = 210  # 210 / 2.54
+        header_cm = 1
+        cam2screen_cm = 1.3
         self.ppcm = ppi/2.54
+        self.reducedRes = screen_res - ul_corner - lr_corner
         self.y_offset_px = (cam2screen_cm + header_cm) * self.ppcm
+        self.pos_scaling = np.array([0.15, 0.5])
+
+        if config == 1:
+            self.y_offset_px = - self.reducedRes[0]/2
+            self.pos_scaling = np.array([0.5, 0.5])
 
         cv2.namedWindow("gaze")
         self.resetGazeMap()
@@ -24,13 +29,14 @@ class visualize:
 
     def resetGazeMap(self):
         self.gazeMap = np.zeros(tuple(np.append(self.reducedRes,3)), np.uint8)
-        cv2.circle(self.gazeMap, tuple(np.flip(self.reducedRes,0)/2), 5, (255,255,255), -1)
+        point_pos = tuple(np.flip(self.reducedRes*self.pos_scaling,0).astype(np.int))
+        cv2.circle(self.gazeMap, point_pos, 5, (255,255,255), -1)
 
     def showGazeMap(self):
         cv2.imshow("gaze", self.gazeMap)
         cv2.waitKey(5)
 
-    def newVis(self, coords):
+    def showGaze(self, coords):
         coords[1] = -coords[1]
         coords = np.round(coords * self.ppcm + np.array([self.reducedRes[1]/2, -self.y_offset_px])).astype(int)
         print(coords)
